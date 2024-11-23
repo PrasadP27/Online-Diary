d410a0c87bc2bc118b530da82538e9e2ea13929a
PrasadP27 <bobpanchal948@gmail.com>
2024-11-23 16:19:14 +0530

    Minor changes

--- server.js
+++ server.js
@@ -2,12 +2,26 @@ const express = require('express');
 const mysql = require('mysql')
 const app = express()
 
+// Middleware for parsing JSON
+app.use(express.json());
+
+// MySQL database connection 
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'temp'
 })
+
+// connect and log the state of connection 
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the database');
+});
+
 
 app.get('/', (req, res) => {
     res.send("Online diary")
@@ -22,6 +36,7 @@ app.get("/books", (req, res) => {
     })
 })
 
+// connection listen 
 app.listen(8080, () => {
     console.log("Express Running on http://localhost:8080");
 })
