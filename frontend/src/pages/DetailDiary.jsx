36bea52e2bf06d1d79f1d91071f2bc472fee9f47
PrasadP27 <bobpanchal948@gmail.com>
2024-12-04 12:30:31 +0530

    Heading styles

--- frontend/src/pages/DetailDiary.jsx
+++ frontend/src/pages/DetailDiary.jsx
@@ -12,6 +12,7 @@ const DetailDiary = () => {
   const [values, setValues] = useState({ heading: "", content: "" });
   const [loading, setLoading] = useState(true);
   const [isSaving, setIsSaving] = useState(false);
+  const [hedingError, setHeadingError] = useState("");
 
   useEffect(() => {
     const createDiary = async () => {
@@ -40,12 +41,12 @@ const DetailDiary = () => {
 
   const toolbarOptions = [
     ["bold", "italic", "underline", "strike"], // toggled buttons
-    ["blockquote", "code-block"],
+    // ["blockquote", "code-block"],
     // ["link", "image", "video", "formula"],
 
     [{ header: 1 }, { header: 2 }], // custom button values
     [{ list: "ordered" }, { list: "bullet" }, { list: "check" }],
-    [{ script: "sub" }, { script: "super" }], // superscript/subscript
+    // [{ script: "sub" }, { script: "super" }], // superscript/subscript
     [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
     [{ direction: "rtl" }], // text direction
 
@@ -87,33 +88,77 @@ const DetailDiary = () => {
     return <div>Loading...</div>; // Show loading state
   }
 
+  // limit heading
+  const headingLimit = (e) => {
+    const newHeading = e.target.value;
+
+    if (newHeading.length <= 30) {
+      setValues({ ...values, heading: newHeading });
+      setHeadingError("");
+    } else {
+      setHeadingError("Max length reached (30 characters)");
+    }
+  };
+
   return (
     <section>
-      <div className=" bg-primary rounded-2xl px-14 py-10 max-w-5xl mx-auto mt-8 border-2 shadow-lg">
+      <div className=" bg-primary rounded-2xl px-10 py-8 max-w-5xl mx-auto mt-8 border-2 shadow-lg">
         {/* <h2 className="font-unbounded text-3xl font-medium first-letter:uppercase">
           heading heasdinf asdhf haosdifh oasidhf
         </h2> */}
-        <textarea
-          name="heading"
-          value={values.heading}
-          onChange={(e) => setValues({ ...values, heading: e.target.value })}
-          className="bg-blue-100 font-unbounded text-3xl font-medium first-letter:uppercase"
-        >
-          {values.heading}
-        </textarea>
-        <ReactQuill
-          modules={module}
-          value={values.content}
-          onChange={(content) => setValues({ ...values, content })}
-        />
+        <div className="flex items-center justify-between mb-5">
+          <div className="px-3 py-0 bg-indigo-100 rounded-full inline-block font-unbounded font-light text-xs tracking-widest">
+            <span className="font-normal">Sept 27 ' 2024</span> : 12:23 am
+          </div>
+          <div className="px-3 py-0 bg-indigo-100 rounded-full inline-block font-unbounded font-light text-xs tracking-widest">
+            <span className="font-normal">120</span> words
+          </div>
+        </div>
 
-        <button
+        <div className="flex items-center justify-between">
+          <div className="flex flex-col items-start w-4/5">
+            <textarea
+              name="heading"
+              value={values.heading}
+              onChange={headingLimit}
+              placeholder="Enter your heading"
+              spellCheck="false"
+              autoCorrect="true"
+              autoComplete="true"
+              rows={1}
+              className="font-unbounded font-medium text-3xl resize-none  w-full focus:outline-none border-b-2 border-solid border-gray-300 p-2 whitespace-nowrap overflow-hidden"
+            >
+              {values.heading}
+            </textarea>
+            <h2 className="error">{hedingError}</h2>
+          </div>
+          <div className="flex flex-col items-center w-1/5">
+            <div className="simple-btn w-4/5 text-center">Save</div>
+            {/* <h4 className="error">error</h4> */}
+          </div>
+        </div>
+
+        <div className="tags flex items-center">
+          <div className="tag1"></div>
+          <div className="tag2"></div>
+          <div className="tag3"></div>
+        </div>
+
+        {/* <div className="editor">
+          <ReactQuill
+            modules={module}
+            value={values.content}
+            onChange={(content) => setValues({ ...values, content })}
+          />
+        </div> */}
+
+        {/* <button
           className="simple-btn"
           onClick={handleSaveClick}
           disabled={isSaving}
         >
           {isSaving ? "Saving..." : "Save"}
-        </button>
+        </button> */}
       </div>
     </section>
   );
