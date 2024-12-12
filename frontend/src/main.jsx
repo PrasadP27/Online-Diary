bee0279a40488509ce6e44cf8de72dbc31450639
PrasadP27 <bobpanchal948@gmail.com>
2024-12-13 00:16:05 +0530

    Added smooth scrolling

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,15 +3,18 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import "./index.css";
 import App from "./App.jsx";
+import { ReactLenis, useLenis } from "@studio-freight/react-lenis";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route>
-          <Route path="/*" element={<App />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ReactLenis root>
+      <BrowserRouter>
+        <Routes>
+          <Route>
+            <Route path="/*" element={<App />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ReactLenis>
   </StrictMode>
 );
