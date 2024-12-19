f3d67a4aada6d98ae6c1b2f3e67ef4158e0c2e7d
PrasadP27 <bobpanchal948@gmail.com>
2024-12-19 21:20:46 +0530

    Added animation

--- frontend/src/component/PageNotFound.jsx
+++ frontend/src/component/PageNotFound.jsx
@@ -1,16 +1,27 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 import React from "react";
 import { Link } from "react-router";
 
 const PageNotFound = () => {
+  useGSAP(() => {
+    gsap.from(".ani", {
+      scale: 0.96,
+      y: 50,
+      opacity: 0,
+      duration: 0.6,
+      stagger: 0.3,
+    });
+  });
   return (
-    <section className="flex flex-col items-center justify-center h-dvh w-full max-h-[900px] text-center">
-      <h1 className="text-[10rem] mb-7">
-        4<span className=" text-indigo-400 dark:text-indigo-700">0</span>4
+    <section className="flex flex-col items-center justify-center h-dvh w-full max-h-[900px] text-center text-secondary dark:text-darkPrimary transition duration-500">
+      <h1 className="ani text-[10rem] mb-7">
+        4<span className="text-indigo-400 dark:text-indigo-700">0</span>4
       </h1>
-      <h3 className="font-unbounded font-medium leading-snug text-4xl text-left dark:text-darkPrimary underline-text-highlight">
+      <h3 className="ani font-unbounded font-medium leading-snug text-4xl text-left dark:text-darkPrimary underline-text-highlight">
         Page not found
       </h3>
-      <h5 className="mb-8 font-nunito font-light text-2xl mt-4 leading-relaxed dark:text-gray-400">
+      <h5 className="ani mb-8 font-nunito font-light text-2xl mt-4 leading-relaxed dark:text-gray-400">
         Page you are looking for is here.
       </h5>
       <div className="">
