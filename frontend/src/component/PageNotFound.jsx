a5fb8bf73c128d813e300fb335450e777807f2fa
PrasadP27 <bobpanchal948@gmail.com>
2024-12-24 17:06:38 +0530

    Responsive page

--- frontend/src/component/PageNotFound.jsx
+++ frontend/src/component/PageNotFound.jsx
@@ -15,20 +15,20 @@ const PageNotFound = () => {
   });
   return (
     <section className="flex flex-col items-center justify-center h-dvh w-full max-h-[900px] text-center text-secondary dark:text-darkPrimary transition duration-500">
-      <h1 className="ani text-[10rem] mb-7">
+      <h1 className="ani text-7xl sm:text-8xl md:text-[10rem] mb-7">
         4<span className="text-indigo-400 dark:text-indigo-700">0</span>4
       </h1>
-      <h3 className="ani font-unbounded font-medium leading-snug text-4xl text-left dark:text-darkPrimary underline-text-highlight">
+      <h3 className="text-xl font-medium leading-snug text-left sm:text-2xl md:text-4xl ani font-unbounded dark:text-darkPrimary underline-text-highlight">
         Page not found
       </h3>
-      <h5 className="ani mb-8 font-nunito font-light text-2xl mt-4 leading-relaxed dark:text-gray-400">
+      <h5 className="mt-4 mb-8 text-base font-light leading-relaxed sm:text-xl md:text-2xl ani font-nunito dark:text-gray-400">
         Page you are looking for is here.
       </h5>
       <div className="">
-        <Link to={"/"} className="simple-btn m-5">
+        <Link to={"/"} className="m-5 simple-btn">
           Home
         </Link>
-        <Link to={"/register"} className="simple-btn m-5">
+        <Link to={"/register"} className="m-5 simple-btn">
           Register
         </Link>
       </div>
